839ca7e5ed0f938e9e6a9390c3bee9a1d30a4911
Wes A. Duff <slysop@gmail.com>
2014-11-13 11:45:40 -0600

    init

---
+++ client/app.js
@@ -0,0 +1,46 @@
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ui.router', 'mgcrea.ngStrap', 'satellizer'])
+  .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/home.html'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'partials/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .state('logout', {
+        url: '/logout',
+        template: null,
+        controller: 'LogoutCtrl'
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'partials/profile.html',
+        controller: 'ProfileCtrl',
+        resolve: {
+          authenticated: ['$location', '$auth', function($location, $auth) {
+            if (!$auth.isAuthenticated()) {
+              return $location.path('/login');
+            }
+          }]
+        }
+      });
+
+    $urlRouterProvider.otherwise('/');
+
+    $authProvider.facebook({
+      clientId: '657854390977827'
+    });
+
+    $authProvider.google({
+      clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
+    });
+
+  });
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/Hack01',
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '536cd79e9dec1b82b3c134e449f8c3d7',
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'FjuwQ-ZmGLi9ZGpXkvlMgshP',
+};
